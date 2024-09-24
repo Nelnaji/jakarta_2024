664cfdded4a16465c350a0abcfb37de341523a74
nel <nawfal.elkhaznagi@gmail.com>
2024-09-24 15:49:58 +0200

    finished for the day

--- src/main/java/demo/jakarta/utils/DataInitializer.java
+++ src/main/java/demo/jakarta/utils/DataInitializer.java
@@ -27,12 +27,9 @@ public class DataInitializer implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
 
 
-
         PlaneType planeType1 = new PlaneType("boeing 134", "boeing", 741.7, 145);
         PlaneType planeType2 = new PlaneType("Airbus 420", "Airbus", 1500, 245);
         PlaneType planeType3 = new PlaneType("boeing 101", "boeing", 370, 58);
-
-
 
 
         Plane plane1 = new Plane(
@@ -40,7 +37,8 @@ public class DataInitializer implements ServletContextListener {
                 planeType1
         );
         Plane plane2 = new Plane(
-                "456", 456, null, planeType2
+                "456", 456, null,
+                planeType2
         );
         Plane plane3 = new Plane(
                 "789", 2, null,
@@ -50,7 +48,6 @@ public class DataInitializer implements ServletContextListener {
         planeRepository.save(plane1);
         planeRepository.save(plane2);
         planeRepository.save(plane3);
-
 
 
     }
